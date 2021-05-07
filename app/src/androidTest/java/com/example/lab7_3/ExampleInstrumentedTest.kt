f80bd0438ef2a6845aa38c3db1200396cbe170ed
Violetta Mamykina <violetta-1998m@mail.ru>
2021-05-07 16:57:13 +0300

    check test

--- app/src/androidTest/java/com/example/lab7_3/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lab7_3/ExampleInstrumentedTest.kt
@@ -1,25 +1,16 @@
 package com.example.lab7_3
 
-import android.content.Context
-import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.os.SystemClock
+
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiObject
-import androidx.test.uiautomator.UiSelector
 import org.hamcrest.CoreMatchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import org.junit.Rule
 
 /**
@@ -31,28 +22,15 @@ import org.junit.Rule
 class ExampleInstrumentedTest {
     @get:Rule
     val rule = activityScenarioRule<MainActivity>()
-//    val mainRule = ActivityTestRule(MainActivity::class.java)
-//    private lateinit var device: UiDevice
 
 
     @Test
     fun broadcastTest() {
         val scenario = rule.scenario
-//        mainRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         Espresso.onView(ViewMatchers.withId(R.id.text))
             .check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")))
-        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.text))
-            .check(ViewAssertions.matches(ViewMatchers.withText(containsString("/"))))
-        //надо?
-//        val context: Context = InstrumentationRegistry.getInstrumentation().context
-//        val intent = context.packageManager.getLaunchIntentForPackage("com.example.lab7_2")
-//        intent?.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//        context.startActivity(intent)
-//        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-//        //instance 1, потому что название lab7_2 тоже TextView
-//        val pathText: UiObject = device.findObject(UiSelector().className("android.widget.TextView").instance(1))
-//        SystemClock.sleep(1000)
-//        assertTrue(pathText.text.contains("/data/"))
+//        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.text))
+//            .check(ViewAssertions.matches(ViewMatchers.withText(containsString("/"))))
     }
 }
